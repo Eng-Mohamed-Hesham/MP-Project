f7215f1ae5590bff0d03d4463fddabaf8e8bfe39
Eng-Mohamed-Hesham <muhammed195910@feng.bu.edu.eg>
2022-12-21 03:56:28 +0200

    Update C file (delete and show functions) and Update Print.asm file

--- Mp-proj.c
+++ Mp-proj.c
@@ -9,67 +9,87 @@ void main(void)
 {
     printf("Enter the number of students(DOES NOT EXCEED 1000): ");
     scanf("%hu", &number_of_students);
-    for (int i = 0; i < number_of_students; i++)
+    for (int i = 1; i <= number_of_students; i++)
     {
-        printf("Enter the ID of student[%hu]: ", i + 1);
+        printf("Enter the ID of student[%hu]: ", i);
         scanf("%hu", &x);
-        printf("Enter the mark of student[%hu]: ", i + 1);
+        printf("Enter the mark of student[%hu]: ", i);
         scanf("%hu", &arr_of_students[x]);
     }
     printf("*******************Stored Marks***********************\n");
-    label:
-        printf("choose (1) to show your list of students:\nchoose (2) to show grade of student using Id of student:\nchoose (3) to add Mark for student:\nchoose (4) to modify grade of student:\nchoose another to exit:\n");
-        unsigned int choose;
-        scanf("%u", &choose);
-        switch (choose)
-        {
-        case 1:
-            show_grades(arr_of_students);
-            goto label;
-            break;
-        case 2:
-            show_mark(arr_of_students);
-            goto label;
-            break;
-        case 3:
-            Add_Student(arr_of_students);
-            goto label;
-            break;
-        case 4:
-            modify_mark(arr_of_students);
-            goto label;
-            break;
-        case 5:
-            return;
-            break;
-        default:
-            break;
-        }
+label:
+    printf("choose (1) to show your list of students:\nchoose (2) to show grade of student using Id of student:\nchoose (3) to add Mark for student:\nchoose (4) to modify grade of student:\nchoose another to exit:\n");
+    unsigned int choose;
+    scanf("%u", &choose);
+    switch (choose)
+    {
+    case 1:
+        show_grades(arr_of_students);
+        goto label;
+        break;
+    case 2:
+        show_mark_for_one_student(arr_of_students);
+        goto label;
+        break;
+    case 3:
+        Add_Student(arr_of_students);
+        goto label;
+        break;
+    case 4:
+        modify_mark(arr_of_students);
+        goto label;
+        break;
+    case 5:
+        delet_student(arr_of_students);
+        goto label;
+        break;
+    case 6:
+        return;
+        break;
+    default:
+        break;
+    }
 }
 
 void show_grades(unsigned short arr[])
 {
-    for (int i = 0; i < number_of_students; i++)
+    for (int i = 1; i <= number_of_students; i++)
     {
-        if(arr[i] != -1)    printf("student[%hu] id is %hu \t mark is %hu \n", i + 1, arr[i], arr[i]);
+        if(arr[i] != -1)    printf("student[%hu] id is %hu \t mark is %hu \n", i, i, arr[i]);
     }
+    printf("**************************************************\n");
 }
 
-
-
-void show_mark(unsigned short arr[])
+void show_mark_for_one_student(unsigned short arr[])
 {
     printf("Enter the ID of student to show: \n");
     scanf("%d", &x);
     if(arr[x])    printf("student id is %hu and his mark is %hu \n", x, arr[x]);
     else    printf("No grade for this student.\n");
+    printf("*******************Stored Marks***********************\n");
 }
+
+void delet_student (unsigned short arr[])
+{
+    printf("Enter the ID of student you want to delete: \n");
+    scanf("%d", &x);
+    arr[x] = 0;
+    for (int i = x; i <= number_of_students-2; i++)
+    {
+        arr[x] = arr[x+1];
+    }
+    number_of_students--;
+        printf("*******************student with id [%hu] has been Deleted!***********************\n", x);
+
+}
+
 void modify_mark(unsigned short arr[])
 {
     int y;
     printf("Enter the ID of student to show: \n");
     scanf("%d", &x);
-    if(arr[x] != x){
+    if(arr[x] != x)
+    {
         unsigned short temp = arr[x];
         printf("Enter the new grade of student to modify: \n");
         scanf("%d", &y);
@@ -79,13 +99,16 @@ void modify_mark(unsigned short arr[])
     else    printf("No grade for this student.\n");
 }
 
-void Add_Student(unsigned short arr[]){
+void Add_Student(unsigned short arr[])
+{
     printf("Enter the ID of student to Add: \n");
     scanf("%d", &x);
-    if(arr[x]){
+    if(arr[x])
+    {
         printf("There is a grade for this student\n");
     }
-    else{
+    else
+    {
         int y;
         printf("Enter the grade for this student.\n");
         scanf("%d", &y);
--- Print.asm
+++ Print.asm
@@ -1,11 +1,28 @@
+
 ORG 100h
-Print_students PROC
-    LEA AL, array_of_students
-    
-    
-    
-    
-    
-    
-RET   ;Return to the caller
-Print_students ENDP  ;End of the procedure
+
+.CODE  
+
+    Print_students PROC
+        
+        MOV CX, num_of_students   ; set the loop iterations
+        LEA SI, array_name        ; load addres of the array of students   ;should be in ASCII
+                    
+                                
+      next_value:
+        
+        MOV DL,a[SI]              ; get value from the array
+        mov AH, 2
+        int 21h
+        inc SI
+        MOV DL, 0AH               ; New line
+        MOV AH, 02H  
+        INT 21H
+        MOV DL, 0DH
+        MOV AH, 02H  
+        INT 21H  
+                                  ; next BYTE
+      LOOP next_value             ; CX++
+        
+    RET                           ; Return to the caller
+    Print_students ENDP           ; End of the procedure
