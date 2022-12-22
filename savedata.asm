49a1af2b50421511ee6fc22142c167e2feb086af
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2022-12-23 01:05:11 +0200

    Updated savedata.asm

--- savedata.asm
+++ savedata.asm
@@ -4,43 +4,45 @@
 .DATA
     fname  DB 'grades.txt', 0
     handle DW ?
-    arr    DW 47, 52, 63, 58, 21
+    arr    DB 4, 7, ' ', 5, 2, ' ', 6, 3, ' ', 5, 8, ' ', 2, 1, ' '
+    digit  DB ?
     space  DB 20H
-    V      DW DUP(0) 
+     
 .CODE
-
     MAIN PROC
-        ; initialize Data Segment
         MOV AX, @DATA
-        MOV DS, AX
+        MOV DS, AX 
         
-        ; Open File for write mode
         CALL Open
-        MOV SI, OFFSET arr
-        MOV DI, 5
-         
+        
+        MOV SI, 0     
+        MOV DI, 15
+        
         loopArr:
             CMP DI, 0
-            JZ  endloop
-            XOR AX, AX
-            MOV AX, [SI]
-            CALL SplitNum  
+            JZ  endloop 
+            MOV AL, arr[SI] 
             
             MOV AH, 40H
-            XOR BX, BX
             MOV BX, handle
-            MOV CX, 1
-            MOV DX, OFFSET space
-            INT 21H  
-            
-            ADD SI, 2
-            DEC DI 
-            JMP loopArr
+            MOV CX, 1 
+            CMP AL, ' ' 
+            JZ takespace
+                ADD AL, 48 
+                MOV OFFSET(digit), AL 
+                MOV DX, OFFSET(digit)
+                JMP CONT 
+            takespace:
+                MOV DX, OFFSET(space)
+            CONT: 
+                INT 21H
+                INC SI 
+                DEC DI 
+                JMP loopArr
             
         endloop:     
             CALL Close
-            
-        ;----- exiting -----;
+
         MOV AH, 4CH
         INT 21H
         
@@ -49,45 +51,11 @@
     Open PROC  
         MOV AH, 3DH
         MOV DX, OFFSET(fname)      
-        MOV AL, 1 ; => for writing mode
+        MOV AL, 1
         INT 21H
         MOV handle, AX
     Open ENDP
     RET
-        
-    SplitNum PROC		
-    	MOV CX,0
-    	MOV DX,0
-    	pushDigit:
-    		CMP AX, 0
-    		JZ popDigit	
-    		MOV BX, 10	
-    		DIV BX				
-    		PUSH DX			
-    		INC CX			
-    		XOR DX, DX
-    		JMP pushDigit
-    	popDigit:
-    		CMP CX,0
-    		JZ exit 
-    		XOR DX, DX
-    		POP DX
-    		ADD DX, 48  
-    		MOV OFFSET V, DX
-    		PUSH CX
-    		MOV AH, 40H
-            XOR BX, BX
-            MOV BX, handle
-            MOV CX, 1
-            MOV DX, OFFSET V
-            INT 21H 
-            POP CX
-    		DEC CX
-    		JMP popDigit
-    		
-    exit:
-        SplitNum ENDP
-        ret
     
     Close PROC
         MOV AH, 3EH
