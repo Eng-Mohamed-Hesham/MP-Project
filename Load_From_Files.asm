1766566260283840de617b66276425cf70478988
mostafa-shoman <mustafa196043@feng.bu.edu.eg>
2022-12-23 13:33:18 +0200

    Edit Load_from_files function

--- Load_From_Files.asm
+++ Load_From_Files.asm
@@ -2,7 +2,9 @@ include 'EMU8086.INC'
 .model small
 .stack 100
 .data 
-
+    buff        db  26        ;MAX NUMBER OF CHARACTERS ALLOWED (25).
+            db  ?         ;NUMBER OF CHARACTERS ENTERED BY USER.
+            db  26 dup(0) ;CHARACTERS ENTERED BY USER.
     fname1 dw "Grades.txt",0
     text db 100  dup(0)     
     fhand dw ?
@@ -42,7 +44,64 @@ EXIT:
     mov ah,9
     lea dx,text
     int 21h  
+    printn ""    
+    
+    ; /* edit in array */
+    
+           
+ ; /* take input */ ;
+ 
+ 
+;CAPTURE STRING FROM KEYBOARD.                                    
+    mov ah, 0Ah ;SERVICE TO CAPTURE STRING FROM KEYBOARD.
+    mov dx, offset buff
+    int 21h                 
+
+;CHANGE CHR(13) BY '$'.
+    mov si, offset buff + 1 ;NUMBER OF CHARACTERS ENTERED.
+    mov cl, [ si ] ;MOVE LENGTH TO CL.
+      
+      
+ ; /* Add input in array */ ;
+    mov al, buff[2]
+    mov text[0], al
+    mov al, buff[3]
+    mov text[1], al
+    printn ""  
     printn ""
+    mov ah,9
+    lea dx,text
+    int 21h
+    
+    
+    xor ax, ax
+    mov al, text[0]
+    sub ax, 48
+    
+    mov bx, 10         
+    mov si, offset numS+5
+    
+    cmp ax,0
+    jge next
+ next:  
+    mov dx,0
+    div bx
+    add dl, 30h
+    mov [si],   dl
+    dec si
+    cmp ax, 0
+    jne next
+    
+    cmp num,0
+    jge sof
+    mov byte ptr[si],   '-'
+    dec si
+    
+sof:
+    inc si
+    mov ah, 9
+    mov dx, si
+    int 21h
     
     mov ah,4ch
     int 21h
